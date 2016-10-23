eee5d77eaa467934bf7ebd7d39dc6519be8c45aa
Arjen Schwarz <arjen.schwarz@bulletproof.net>
2016-10-23 14:53:06 +1100

    Don't try to decrypt twice

--- shared/sender.go
+++ shared/sender.go
@@ -7,7 +7,7 @@ import (
 )
 
 // SendToIFTTT sends the provided contents to IFTTT
-func SendToIFTTT(encryptedIFTTTKey string, iftttTriggerName string, contents []string) {
-	makerURL := fmt.Sprintf("https://maker.ifttt.com/trigger/%s/with/key/%s", iftttTriggerName, DecryptKMS(encryptedIFTTTKey))
+func SendToIFTTT(iftttKey string, iftttTriggerName string, contents []string) {
+	makerURL := fmt.Sprintf("https://maker.ifttt.com/trigger/%s/with/key/%s", iftttTriggerName, iftttKey)
 	http.PostForm(makerURL, url.Values{"value1": contents})
 }
